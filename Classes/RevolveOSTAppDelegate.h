f62e16eba3475f0db1fb4b5a32b2d581a1bbdcaa
Johan Halin <johan.halin@karppinen.fi>
2010-11-18 16:52:37 +0200

    First commit

---
+++ Classes/RevolveOSTAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  RevolveOSTAppDelegate.h
+//  RevolveOST
+//
+//  Created by Johan Halin on 18.11.2010.
+//  Copyright Parasol 2010. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RevolveOSTAppDelegate : NSObject <UIApplicationDelegate> {
+	UIWindow *window;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+
+@end
